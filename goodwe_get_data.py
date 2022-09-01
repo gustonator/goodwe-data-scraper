4f3540d5864b28eaec814e4612e3fdaa96f442f2
gustonator <rudolf.brosztl@gmail.com>
2022-09-01 08:05:21 +0200

    added absolute path to log file

--- goodwe_get_data.py
+++ goodwe_get_data.py
@@ -2,14 +2,18 @@ import asyncio
 import goodwe
 from datetime import date
 
+#Config
 ip_address = '192.168.2.35' #Add here the IP adress of the inverter
+logfilePath = "/data/goodwe-data-scraper"
+
+
 current_date = str(date.today())
 
 async def get_runtime_data():
     inverter = await goodwe.connect(ip_address)
     runtime_data = await inverter.read_runtime_data()
 
-    with open('data-'+current_date+'.txt', 'w') as f:
+    with open(logfilePath+'/data-'+current_date+'.txt', 'w') as f:
         for sensor in inverter.sensors():
             if sensor.id_ in runtime_data:
                 #print(f"{sensor.id_}: \t\t {sensor.name} = {runtime_data[sensor.id_]} {sensor.unit}")
